8a5f8524048702d5a2bab70ef303e7f340a9d0c9
k16wire <k16wire@gmail.com>
2019-01-16 01:49:20 +0900

    rf test case

--- src/test/java/com/sw300/acme/course/CourseTest.java
+++ src/test/java/com/sw300/acme/course/CourseTest.java
@@ -1,6 +1,6 @@
-package com.sw300.acme;
+package com.sw300.acme.course;
 
-import com.sw300.acme.course.*;
+import com.sw300.acme.AcmeApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
