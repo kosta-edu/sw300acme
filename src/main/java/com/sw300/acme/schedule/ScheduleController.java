69ec5cce4a170275321ef3aaf3196f61d80b9c69
Sung-Yeol Yun <existmaster@naver.com>
2019-01-27 17:14:10 +0900

    Create Service Controller

--- src/main/java/com/sw300/acme/schedule/ScheduleController.java
+++ src/main/java/com/sw300/acme/schedule/ScheduleController.java
@@ -2,14 +2,12 @@ package com.sw300.acme.schedule;
 
 import com.sw300.acme.clazz.Clazz;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/schedule-service")
 public class ScheduleController {
 
     private ScheduleService scheduleService;
@@ -25,5 +23,33 @@ public class ScheduleController {
     ) {
         final List<Clazz> clazzes = scheduleService.scheduledClasses(courseId);
         return "clazzes";
+    }
+
+    @RequestMapping(value = "/course-clazzes", method = RequestMethod.GET)
+    public List<Clazz> clazzes(
+            @RequestParam(value="courseId") Long courseId
+    ) {
+        final List<Clazz> classes = scheduleService.detailOfClassesForCourse(courseId);
+        return classes;
+    }
+
+    @RequestMapping(value = "/clazzes-detail", method = RequestMethod.GET)
+    public Clazz clazzeDetails(
+            @RequestParam(value="clazzId") Long clazzId
+    ) {
+        final Clazz classDetails = scheduleService.detailOfClass(clazzId);
+        return classDetails;
+    }
+
+    @RequestMapping(value = "/associate-instructor", method = RequestMethod.POST)
+    public void associateInstructor(@RequestBody InstructorInfo instructorInfo
+    ) {
+        scheduleService.associateInstructorForClassDay(instructorInfo.getClassDayId(), instructorInfo.getInstructorId());
+    }
+
+    @RequestMapping(value = "/associate-venu", method = RequestMethod.POST)
+    public void associateVenu(@RequestBody VenuInfo venuInfo
+    ) {
+        scheduleService.associateVenuForClassDay(venuInfo.getClassDayId(), venuInfo.getVenuId());
     }
 }
