3a74767f1eedfb4ba643d6bf1922413676753503
k16wire <k16wire@gmail.com>
2019-04-02 16:22:45 +0900

    add alias of api

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8090',
+                target: 'http://acmeapi:8090',
                 ws: true,
                 changeOrigin: true
             }
