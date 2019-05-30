9074488690c6daf4ab71bf677acb59deeb6191ca
jyjang <jyjang@uengine.org>
2019-05-30 16:23:55 +0900

    changes are lot

--- src/main/java/com/sw300/acme/course/CourseRegistered.java
+++ src/main/java/com/sw300/acme/course/CourseRegistered.java
@@ -3,11 +3,14 @@ package com.sw300.acme.course;
 import com.sw300.acme.clazz.ClassDayRegistered;
 import com.sw300.acme.customer.Enrolled;
 
+import java.util.Date;
+
 /**
  * Created by uengine on 2019. 5. 30..
  */
 public class CourseRegistered extends ClassDayRegistered {
     public CourseRegistered(Course course) {
+        setDate(new Date());
         setTitle(course.getTitle());
     }
 }
